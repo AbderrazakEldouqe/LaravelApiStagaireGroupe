f372e36a344a68e849c82bd5aaeb8ba91e57c8ce
healingfield <dihratene@gmail.com>
2020-10-01 22:04:19 +0100

    migration

--- app/Http/Controllers/GroupeController.php
+++ app/Http/Controllers/GroupeController.php
@@ -6,5 +6,9 @@ use Illuminate\Http\Request;
 
 class GroupeController extends Controller
 {
-    //
+   /* protected $user;
+    public function __construct()
+    {
+        $this->user = JWTAuth::parseToken()->authenticate();
+    }*/
 }
